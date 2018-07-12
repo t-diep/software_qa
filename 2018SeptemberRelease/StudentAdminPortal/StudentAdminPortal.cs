0cb9868d47c543c69dd26ab33d349e35b7dfcd30
Tony Diep <anthony.tony.diep@gmail.com>
2018-07-12 10:57:14 -0600

    Finished automation test for SAMS-915

--- 2018SeptemberRelease/StudentAdminPortal/StudentAdminPortal.cs
+++ 2018SeptemberRelease/StudentAdminPortal/StudentAdminPortal.cs
@@ -58,7 +58,7 @@ namespace StudentAdminPortal
             driver.FindElement(By.LinkText("Students")).Click();
 
             IWebElement acctNumberField = driver.FindElement(By.Id("accountNumber"));
-            acctNumberField.SendKeys("RS19100172");
+            acctNumberField.SendKeys("RS19100177");
             acctNumberField.SendKeys(Keys.Enter);
             driver.FindElement(By.CssSelector("body.pace-done:nth-child(2) section.theme-default section.main-content-wrapper:nth-child(3) div.row.student-search div.col-md-12 div.panel.panel-success:nth-child(2) div.col-lg-12.background_main_bg div.bs-example div.dataTables_wrapper.no-footer table.display.dataTable.no-footer tbody:nth-child(2) tr.odd td.sorting_1:nth-child(1) > a.btn.btn-primary")).Click();
 
@@ -66,14 +66,52 @@ namespace StudentAdminPortal
             IWebElement appInfo = waitForAppInfo.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("/html[1]/body[1]/section[1]/section[1]/div[2]/div[1]/div[1]/div[1]/ul[1]/li[3]/a[1]")));
             appInfo.Click();
 
-            //IWebElement innerScrollMenu = driver.FindElement(By.XPath("/html[1]/body[1]/section[1]/section[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/form[1]/div[2]"));
-            //innerScrollMenu.Click();
+            IWebElement reviewStatusLabel = driver.FindElement(By.XPath("/html[1]/body[1]/section[1]/section[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/form[1]/div[1]/div[1]/div[1]/h4[1]/span[1]"));
 
-            Actions scrollDown = new Actions(driver);
+            if(reviewStatusLabel.Text == "Not Started")
+            {
+                Actions scrollDown = new Actions(driver);
 
-            IWebElement fafsaDateField = driver.FindElement(By.Id("fafsaDocument.statusDate"));
-            scrollDown.MoveToElement(fafsaDateField);
-            scrollDown.Perform();
+                /*INITIAL HIGH SCHOOL TRANSCRIPT DATES*/
+                IWebElement postMarkHSTranscript = driver.FindElement(By.Id("initialHighSchoolTranscriptDocument1.postmarkDate"));
+                scrollDown.MoveToElement(postMarkHSTranscript);
+                scrollDown.Perform();
+                postMarkHSTranscript.Clear();
+                postMarkHSTranscript.SendKeys("01/02/2019");
+                IWebElement verifiedHSTranscript = driver.FindElement(By.Id("initialHighSchoolTranscriptDocument1.statusDate"));
+                verifiedHSTranscript.Clear();
+                verifiedHSTranscript.SendKeys("01/03/2019");
+                IWebElement initialHSDeliveryMethod = driver.FindElement(By.Id("initialHighSchoolTranscriptDocument1.deliveryMethod"));
+                initialHSDeliveryMethod.SendKeys("Mailed");
+
+                /*INITIAL ACT SCORE DATES*/
+                IWebElement actScoresPostmark = driver.FindElement(By.Id("actScoresDocument.postmarkDate"));
+                scrollDown.MoveToElement(actScoresPostmark);
+                scrollDown.Perform();
+                actScoresPostmark.Clear();
+                actScoresPostmark.SendKeys("01/04/2019");
+                IWebElement actScoresVerified = driver.FindElement(By.Id("actScoresDocument.statusDate"));
+                actScoresVerified.Clear();
+                actScoresVerified.SendKeys("01/05/2019");
+                IWebElement actScoresDeliveryMethod = driver.FindElement(By.Id("actScoresDocument.deliveryMethod"));
+                actScoresPostmark.SendKeys("eData");
+
+                //Click on the Save Review button
+                IWebElement saveReviewButton = driver.FindElement(By.Id("saveReview"));
+                saveReviewButton.Click();
+            }
+            
+            //If necessary, click the Complete Review button to move to Second Review
+            if(reviewStatusLabel.Text != "Second Transcript")
+            {
+                //Click on the Complete Review button
+                IWebElement completeReviewButton = driver.FindElement(By.Id("completeReview"));
+                completeReviewButton.Click();
+            }
+
+            IWebElement reviewerLabel = driver.FindElement(By.XPath("/html[1]/body[1]/section[1]/section[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/form[1]/div[1]/div[1]/div[1]/h4[1]/span[2]"));
+
+            Assert.AreEqual("lsalgado", reviewerLabel.Text);          
         }
     }
 }
